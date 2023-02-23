e707fd9b9bbd594e91eba1da97db73a6847e9b55
김승우 <rnqhqaltjs@naver.com>
2023-02-23 19:55:41 +0900

    [refactor] 의존성 주입

--- app/src/main/java/com/example/chatapp/data/repository/ChatRepositoryImpl.kt
+++ app/src/main/java/com/example/chatapp/data/repository/ChatRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.example.chatapp.data.repository
 
 import android.app.Application
 import android.net.Uri
+import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -58,6 +59,7 @@ class ChatRepositoryImpl(
             override fun onCancelled(error: DatabaseError) {
                 Toast.makeText(application,"유저 리스트를 불러오는데 실패했습니다", Toast.LENGTH_SHORT).show()
                 //실패 시 실행
+                Log.e("MainActivity", error.toException().toString())
             }
         })
     }
