1684a83eb0c16b9d0f19bd8a985f59793bc927a0
김승우 <rnqhqaltjs@naver.com>
2023-08-01 14:43:15 +0900

    [feat] 이미지 다운로드 기능

--- app/src/main/java/com/example/chatapp/ui/detailphoto/DetailPhotoFragment.kt
+++ app/src/main/java/com/example/chatapp/ui/detailphoto/DetailPhotoFragment.kt
@@ -1,7 +1,10 @@
 package com.example.chatapp.ui.detailphoto
 
+import android.app.DownloadManager
+import android.content.Context.DOWNLOAD_SERVICE
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
@@ -9,6 +12,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -54,51 +58,24 @@ class DetailPhotoFragment : Fragment() {
 
 
         binding.downloadPhotoView.setOnClickListener {
-            downloadImage("https://firebasestorage.googleapis.com/v0/b/chatapp-19dab.appspot.com/o/chatImages%2FitOaTszVbRhikiVBoNQWV47tF583HHJ8QyPVlGXrEsKmmr6VXadTlnJ2%2F1690776304728?alt=media&token=ea540030-5285-4ee1-8c3d-dd29829ffc82")
+            toast("다운로드 시작")
+            downloadImage(args.message.photoImage)
+
         }
     }
 
-    private fun downloadImage(imageUrl: String): String? {
-        var filePath: String? = null
-        var bitmap: Bitmap? = null
+    private fun downloadImage(imageUrl: String) {
+        val request = DownloadManager.Request(Uri.parse(imageUrl))
+            .setTitle(args.message.time)
+            .setDescription("다운로드중....")
+            .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+            .setAllowedOverMetered(true)
 
-        try {
-            val loader = ImageLoader(requireContext())
-            val req = ImageRequest.Builder(requireContext())
-                .data(imageUrl) // demo link
-                .target { result ->
-                    val bitmap2 = (result as BitmapDrawable).bitmap
-                    bitmap = bitmap2
-                }
-                .build()
+        val dm = context?.getSystemService(DOWNLOAD_SERVICE) as DownloadManager
 
-            val disposable = loader.enqueue(req)
-
-            // 외부 저장소에 디렉토리 생성
-            val root = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
-            val directory = File(root, "YourDirectory") // 여기서 "YourDirectory"는 저장될 디렉토리 이름으로 원하는 이름으로 변경 가능합니다.
-            if (!directory.exists()) {
-                directory.mkdirs()
-            }
-
-            // 파일 이름 설정
-            val fileName = "YourImage.jpg" // 저장될 파일 이름으로 원하는 이름으로 변경 가능합니다.
-            val file = File(directory, fileName)
-
-            // 이미지 파일 저장
-            val out: OutputStream = FileOutputStream(file)
-            bitmap?.compress(Bitmap.CompressFormat.JPEG, 100, out)
-            out.flush()
-            out.close()
-
-            // 파일 경로 반환
-            filePath = file.absolutePath
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-
-        return filePath
+        dm.enqueue(request)
     }
+
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
