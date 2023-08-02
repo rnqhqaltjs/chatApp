b057bb0ef8260ba6e7482065b23f40069b34924d
김승우 <rnqhqaltjs@naver.com>
2023-08-02 16:13:50 +0900

    [feat] 이미지 다운로드 기능

--- app/src/main/java/com/example/chatapp/ui/detailphoto/DetailPhotoViewModel.kt
+++ app/src/main/java/com/example/chatapp/ui/detailphoto/DetailPhotoViewModel.kt
@@ -1,18 +1,27 @@
 package com.example.chatapp.ui.detailphoto
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.DownloadManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Environment
 import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.ObservableBoolean
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import androidx.navigation.findNavController
-import com.example.chatapp.data.model.User
+import com.example.chatapp.R
+import com.example.chatapp.data.model.Message
 import com.example.chatapp.data.repository.ChatRepository
-import com.example.chatapp.util.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
 import javax.inject.Inject
 
 @HiltViewModel
@@ -28,5 +37,25 @@ class DetailPhotoViewModel @Inject constructor(
 
     fun onDetailPhotoImageClick(currentSwitchState: Boolean) {
         switch.set(!currentSwitchState)
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    fun downloadImage(context: Context, message: Message) {
+        Toast.makeText(context, "다운로드 시작", Toast.LENGTH_SHORT).show()
+        val fileName =
+            "/${context.getString(R.string.app_name)}/${SimpleDateFormat("yyyyMMddHHmmss").format(Date())}.jpg" // 이미지 파일 명
+
+        val req = DownloadManager.Request(Uri.parse(message.photoImage))
+
+        req.setTitle(fileName) // 제목
+            .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED) // 알림 설정
+            .setMimeType("image/*")
+            .setDestinationInExternalPublicDir(
+                Environment.DIRECTORY_PICTURES,
+                fileName
+            ) // 다운로드 완료 시 보여지는 이름
+
+        val manager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        manager.enqueue(req)
     }
 }
