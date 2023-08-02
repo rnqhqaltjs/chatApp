ace483fa35128ac8baff4ed3cc2c3e7f2d87a278
김승우 <rnqhqaltjs@naver.com>
2023-08-02 16:13:28 +0900

    [refactor] 뷰바인딩 -> 데이터바인딩

--- app/src/main/java/com/example/chatapp/ui/detailphoto/DetailPhotoFragment.kt
+++ app/src/main/java/com/example/chatapp/ui/detailphoto/DetailPhotoFragment.kt
@@ -1,34 +1,18 @@
 package com.example.chatapp.ui.detailphoto
 
-import android.app.DownloadManager
-import android.content.Context.DOWNLOAD_SERVICE
-import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import coil.ImageLoader
-import coil.load
-import coil.request.ImageRequest
 import com.example.chatapp.R
 import com.example.chatapp.databinding.FragmentDetailPhotoBinding
-import com.example.chatapp.ui.profile.ProfileViewModel
-import com.example.chatapp.util.toast
 import dagger.hilt.android.AndroidEntryPoint
-import java.io.File
-import java.io.FileOutputStream
-import java.io.OutputStream
 
 @AndroidEntryPoint
 class DetailPhotoFragment : Fragment() {
@@ -55,25 +39,6 @@ class DetailPhotoFragment : Fragment() {
 
         binding.exitPhotoView.bringToFront()
         binding.downloadPhotoView.bringToFront()
-
-
-        binding.downloadPhotoView.setOnClickListener {
-            toast("다운로드 시작")
-            downloadImage(args.message.photoImage)
-
-        }
-    }
-
-    private fun downloadImage(imageUrl: String) {
-        val request = DownloadManager.Request(Uri.parse(imageUrl))
-            .setTitle(args.message.time)
-            .setDescription("다운로드중....")
-            .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-            .setAllowedOverMetered(true)
-
-        val dm = context?.getSystemService(DOWNLOAD_SERVICE) as DownloadManager
-
-        dm.enqueue(request)
     }
 
     override fun onDestroyView() {
